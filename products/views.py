dc1f0c4eadfd9cf3f76f3081960f2e4d2c67e4d7
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-04 15:09:00 -0300

    feat: create Product Permission IsAdminOrReadOnly

--- products/views.py
+++ products/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
 
 class ProductViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
