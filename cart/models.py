0107a4540e7e74b22cb01d0d20cff571ed553850
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-19 08:53:06 -0300

    feat(cart): add cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
