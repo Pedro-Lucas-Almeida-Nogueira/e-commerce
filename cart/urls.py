f1cc40e413be608f882b2700122d2aa817286eb5
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-20 09:41:04 -0300

    feat(cart_item): add cart_item serializer

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,5 @@ from .views import CartListCreateView, CartItemListCreateView
 
 urlpatterns = [
     path('cart/', CartListCreateView.as_view(), name='cart'),
-    path('cart/items/', CartItemListCreateView.as_view(), name='Cart_items'),
+    path('cart/items/', CartItemListCreateView.as_view(), name='cart_items'),
 ]
