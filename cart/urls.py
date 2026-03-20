6e6273d73cafbb814a943073b7e99c67d5f2f5b6
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-20 08:40:47 -0300

    feat(cart_items): add cart_items url

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartListCreateView
+from .views import CartListCreateView, CartItemListCreateView
 
 
 urlpatterns = [
-    path('cart/', CartListCreateView.as_view())
+    path('cart/', CartListCreateView.as_view(), name='cart'),
+    path('cart/items/', CartItemListCreateView.as_view(), name='Cart_items'),
 ]
