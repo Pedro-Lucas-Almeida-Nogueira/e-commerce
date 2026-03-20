4e5f23b82899b0e61a53405c50dfe14485b35a7a
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-20 09:24:19 -0300

    feat(cart_item): add cart_item view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView
-from .models import Cart
-from .serializers import CartSerializer
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
+
 
 class CartListCreateView(ListCreateAPIView):
     queryset = Cart.objects.all()
@@ -11,5 +12,16 @@ class CartListCreateView(ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-   
+
+
+class CartItemListCreateView(ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
+
+    def perform_create(self, serializer):
+        cart = Cart.objects.get(user=self.request.user)
+        serializer.save(cart= cart)
          
