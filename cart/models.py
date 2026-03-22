946d3dfd2ea72da5983e9600cceab99c81cd3c19
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-22 12:10:09 -0300

    feat(cart): add cart method get_total

--- cart/models.py
+++ cart/models.py
@@ -6,6 +6,16 @@ from product.models import Product
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_total(self):
+        total_order_value = 0
+
+        for item in self.items.all():
+            total_price_item = item.product.price * item.quantity
+            total_order_value += total_price_item
+        
+        return total_order_value
+
+            
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
