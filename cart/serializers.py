71dd0c4a9478d2784121f15d41c2c1da56c80caf
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-22 11:34:13 -0300

    feat/cart: add items field to cart serializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
 
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = ['id','user']
-
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ['id', 'cart', 'product', 'quantity', 'created_at', 'updated_at']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id','user','items']
