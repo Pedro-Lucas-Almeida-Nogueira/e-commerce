3cffd7395c37003f12954542e31775cc6d3b7eee
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-03-25 10:11:20 -0300

    feat(authentication): add default IsAuthenticated permission

--- app/settings.py
+++ app/settings.py
@@ -125,5 +125,8 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
